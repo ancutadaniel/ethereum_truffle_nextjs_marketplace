cc4331f763f4ce7588bccf5af1fa56f02bf05041
ancutadaniel <ancutadaniel@gmail.com>
2023-03-14 19:56:44 +0200

    update config

--- truffle-config.js
+++ truffle-config.js
@@ -20,9 +20,22 @@ module.exports = {
         ),
       network_id: 5, // Goerli's id
       gas: 5500000, // Goerli has a lower block limit than mainnet
-      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      confirmations: 2, // # of conf to wait between deployments. (default: 0)
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
+    live: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://mainnet.infura.io/v3/${infuraKey}`
+        ),
+      network_id: 1, // Mainnet's id
+      gas: 2500000,
+      gasPrice: 29000000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
     },
   },
 
